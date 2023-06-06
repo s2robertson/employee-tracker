ee9198de65976e90ba351e88d05d847e38b90b64
Steven Robertson <s2robertson@gmail.com>
2023-06-05 20:17:48 -0400

    converted role id to title, department, and salary in employee query

--- db/db.js
+++ db/db.js
@@ -17,13 +17,18 @@ module.exports = (async function() {
         },
 
         async readRoles() {
-            const [rows, fields] = await conn.query('SELECT r.id, r.title, d.name AS department, r.salary FROM role r INNER JOIN department d ON r.department_id = d.id');
+            const query = 'SELECT r.id, r.title, d.name AS department, r.salary FROM role r '
+                + 'INNER JOIN department d ON r.department_id = d.id'
+            const [rows, fields] = await conn.query(query);
             const rs = new ResultSet(rows, fields);
             console.log(rs.toString());
         },
 
         async readEmployees() {
-            const [rows, fields] = await conn.query('SELECT * FROM employee');
+            const query = 'SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary FROM employee e '
+                + 'INNER JOIN role r ON e.role_id = r.id '
+                + 'INNER JOIN department d ON r.department_id = d.id';
+            const [rows, fields] = await conn.query(query);
             const rs = new ResultSet(rows, fields);
             console.log(rs.toString());
         },
